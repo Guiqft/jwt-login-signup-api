3306ea9ecb7403d0e8d60a1568abfb9edec41c44
Guiqft <g00_deoliveira@outlook.com>
2020-05-31 12:38:01 -0300

    Validating signup inputs and dealing with browser CORS

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -4,18 +4,19 @@ const connection = require('../database/connection');
 //package that we will need in order to salt and hash users passwords.
 const bcrypt = require('bcrypt');
 
+//function to list all the registered users
 const list = async (request, response) => {
     const users = await connection('users').select('*');
-
     return response.json(users);
 };
 
+//make a new user register
 const save = async (user) => {
     const { name, surname, email, password, type } = user;
 
     return await connection('users')
         .insert({
-            createdAt: new Date(),
+            createdAt: new Date().toISOString(),
             name,
             surname,
             email,
@@ -26,17 +27,19 @@ const save = async (user) => {
         .catch((err) => { console.log(err); throw err });
 };
 
-//creating a hash so password can be stored safely in our database.
+//function to make a hash to password, so can be stored safely in our database.
 const hashPassword = async (plaintextPassword) => {
     const saltRounds = 10;
     const salt = await bcrypt.genSalt(saltRounds);
     return await bcrypt.hash(plaintextPassword, salt);
 }
 
+//function to get the user in the database that email matches with the requested email
 const getUserByEmail = (email) =>
     connection('users')
         .where({ email })
         .first()
         .catch((err) => { console.log(err); throw err });
 
+//export modules for external use
 module.exports = { save, list, getUserByEmail }
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,36 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+//to validate inputs
+const { check, validationResult } = require('express-validator')
+
 const routes = express.Router();
 
 const userRepository = require('./repositories/userRepository');
+
+//validating the inputs
+const validationRules = [
+    check('name').isAlphanumeric(),
+    check('surname').isAlphanumeric(),
+    //checking if email is already in use 
+    check('email').isEmail().custom(value => {
+        return userRepository.getUserByEmail(value).then(user => {
+        if (user) {
+            return Promise.reject('E-mail already in use');
+        }
+        })
+    }),
+    check('password').isLength({ min: 8 }),
+    check('type').isAlphanumeric()
+]   
+
+// Options route used for preflight request to the login POST route (cors)
+routes.options("/*", (request, response) => {
+    response.header('access-control-allow-origin', '*');
+    response.header('access-control-allow-methods', 'POST');
+    response.header('access-control-allow-headers', ' Accept, access-control-allow-origin, Content-Type');
+    response.sendStatus(204);
+});
 
 //listing all the users
 routes.get('/auth/signup', async (request, response) => {
@@ -14,8 +41,14 @@ routes.get('/auth/signup', async (request, response) => {
 });
 
 //user signup
-routes.post('/auth/signup', async (request, response) => {
+routes.post('/auth/signup', validationRules, async (request, response) => {
     console.log(request.body);
+
+    //validating the results
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+        return response.status(422).json({ errors: errors.array() });
+    }
 
     await userRepository.save(request.body);
 
@@ -24,6 +57,8 @@ routes.post('/auth/signup', async (request, response) => {
 
 //user login
 routes.post('/auth/login', async (request, response) => {
+    response.header('access-control-allow-origin', '*');
+
     const user = await userRepository.getUserByEmail(request.body.email);
     console.log(user);
 
